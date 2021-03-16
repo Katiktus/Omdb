44ff0af3c9d7ab130b2f3014f15c060721264f6c
Kate <60921902+Katiktus@users.noreply.github.com>
2021-03-16 15:30:49 +0200

    Update SendGetRequestInterface.java

--- src/main/java/ua/edu/sumdu/j2ee/pohorila/parse/model/SendGetRequestInterface.java
+++ src/main/java/ua/edu/sumdu/j2ee/pohorila/parse/model/SendGetRequestInterface.java
@@ -1,2 +1,8 @@
-package ua.edu.sumdu.j2ee.pohorila.parse.model.services;public interface SendGetRequestINterface {
+package ua.edu.sumdu.j2ee.pohorila.parse.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public interface SendGetRequestInterface {
+    public String sendGetRequest(String requestUrl);
 }
